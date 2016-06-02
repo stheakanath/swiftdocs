d2001533ffdeee76246cf2e2fc6caf1966561758
Sony Theakanath <stheakanath@gmail.com>
2016-06-02 16:23:14 -0700

    How to make a custom UIButton

---
+++ CustomButton.swift
@@ -0,0 +1,38 @@
+//
+//  CustomButton.swift
+//  Swift Resources
+//
+//  Created by Kuriakose Theakanath on 6/2/16.
+//  Copyright © 2016 Kuriakose Theakanath. All rights reserved.
+//
+
+//
+//  Intention for this file is to demonstrate how to create a
+//  custom UIButton in Swift.
+//
+//  Initialization: var button = CustomButton() 
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp() 
+    }
+
+    init() {
+        super.init(frame: CGRectZero)
+        setUp()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    func setUp() {
+	// Do your set up here
+    }
+}
